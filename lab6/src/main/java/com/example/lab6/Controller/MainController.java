619690eaa24d4b415bebcc6b5f4d005dcf5d9f4b
blvck29 <a20210850@pucp.edu.pe>
2024-05-23 01:27:03 -0500

    aaa

--- lab6/src/main/java/com/example/lab6/Controller/MainController.java
+++ lab6/src/main/java/com/example/lab6/Controller/MainController.java
@@ -1,13 +1,36 @@
 package com.example.lab6.Controller;
 
+import com.example.lab6.Entity.Dispositivo;
+import com.example.lab6.Repository.RepoDispositivo;
+import com.example.lab6.Repository.RepoReserva;
+import com.example.lab6.Repository.RepoUsuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class MainController {
 
+    final RepoDispositivo repoDispositivo;
+    final RepoUsuario repoUsuario;
+    final RepoReserva repoReserva;
+
+    public MainController(RepoDispositivo repoDispositivo, RepoUsuario repoUsuario, RepoReserva repoReserva) {
+        this.repoDispositivo = repoDispositivo;
+        this.repoUsuario = repoUsuario;
+        this.repoReserva = repoReserva;
+    }
+
     @GetMapping("/dispositivos")
-    public String listaDispositivos(){
+    public String listaDispositivos(Model model){
+
+        ArrayList<Dispositivo> listaDispositivos = new ArrayList<>();
+
+        listaDispositivos = (ArrayList<Dispositivo>) repoDispositivo.findAll();
+
+        model.addAttribute("listaDispositivos",listaDispositivos);
         return "lista_dispositivos";
     }
 
